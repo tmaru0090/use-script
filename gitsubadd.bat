087079ab7b731400707cf7b33cebe9c7e9b90579
tanukimaru0090 <tanusan1015@gmail.com>
2024-12-07 23:44:59 +0900

    Update

---
+++ gitsubadd.bat
@@ -0,0 +1,9 @@
+git subtree add --prefix=use-scripts/wrapper-nvim/ git@github.com:tmaru0090/wrapper-nvim.git maste
+r --squash
+
+
+git subtree add --prefix=use-scripts/add-paths/ git@github.com:tmaru0090/add_paths.git maste
+r --squash
+
+
+
---
+++ gitsubpull.bat
@@ -0,0 +1,8 @@
+git subtree pull --prefix=use-scripts/wrapper-nvim https://github.com/tmaru0090/wrapper-nvim.git m
+aster --squash
+
+git subtree pull --prefix=use-scripts/add-paths/ git@github.com:tmaru0090/add_paths.git maste
+r --squash
+
+
+
